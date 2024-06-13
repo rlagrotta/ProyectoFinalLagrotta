e2bc274fb97cfd79bc1e10fb148a9592c5509599
Roque Lagrotta <roque@rocketpowerstudios.com>
2024-06-12 23:35:14 -0500

    tercer commit

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,117 +1,184 @@
-// Definición del objeto 'tarea'
-let tarea = {
-  sinCompletar: [],
-  Completadas: []
+// Objeto con clientes para probar el for y el while
+let texto = {
+  "clientes": [
+    {
+      "id": 1,
+      "nombre": "Juan Pérez",
+      "cedula": "123456789",
+      "vehiculos": [
+        {
+          "placa": "ABC123",
+          "numero_ticket": "TCKT001",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "nombre": "María López",
+      "cedula": "987654321",
+      "vehiculos": [
+        {
+          "placa": "DEF456",
+          "numero_ticket": "TCKT003",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "nombre": "Carlos García",
+      "cedula": "456123789",
+      "vehiculos": [
+        {
+          "placa": "GHI789",
+          "numero_ticket": "TCKT004",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "nombre": "Roque Lagrotta",
+      "cedula": "8783614",
+      "vehiculos": [
+        {
+          "placa": "ROCK37PWR",
+          "numero_ticket": "TCKT005",
+          "prestamo": "Préstamo de Moto"
+        }
+      ]
+    },
+    {
+      "id": 5,
+      "nombre": "Giovanna Lagrotta",
+      "cedula": "8783615",
+      "vehiculos": [
+        {
+          "placa": "GIOGROTT",
+          "numero_ticket": "TCKT005",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    },
+    {
+      "id": 6,
+      "nombre": "Stefano Fouquet",
+      "cedula": "456123721",
+      "vehiculos": [
+        {
+          "placa": "STEFQT01",
+          "numero_ticket": "TCKT006",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "nombre": "Carlos Diaz",
+      "cedula": "456123754",
+      "vehiculos": [
+        {
+          "placa": "TH3B4TM4N",
+          "numero_ticket": "TCKT007",
+          "prestamo": "Préstamo de Auto"
+        }
+      ]
+    }
+  ]
 };
 
-// Referencia al campo de entrada para añadir tareas
-let inputAnadirField = document.getElementById('anadirInput');
+//db vacia deshabilitada para pruebas de if/else
+/*let texto = {
+  "clientes": []
+};*/
 
-// Función para añadir una nueva tarea a la lista 'sinCompletar'
-function anadirFunc() {
-  let inputAnadir = document.getElementById('anadirInput').value;
 
-  // Verificar si el valor no está vacío
-  if (inputAnadir.trim() !== '') {
-    // Añadir el valor al array 'sinCompletar'
-    tarea.sinCompletar.push(inputAnadir);
+// función con for deshabilitada para habilitar while
+/* function myFunction() {
+  if (texto.clientes.length !== 0) {
+    let contenedorTabla = document.getElementById('contenedorTabla');
+    let html = `<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Id</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Cedula</th>
+      <th scope="col">Placa</th>
+      <th scope="col">Ticket</th>
+      <th scope="col">Tipo</th>
+    </tr>
+  </thead>
+  <tbody>`;
 
-    // Construir la tabla en una cadena
-    let tablaHTML = '<table><tr><th>#</th><th>Tarea</th><th>Opciones</th></tr>';
-    
-    tarea.sinCompletar.forEach((element, index) => {
-      tablaHTML += `<tr>
-        <th scope="row">${index + 1}</th>
-        <td>${element}</td>
-        <td><button type="button" class="btn btn-outline-secondary btn-block" onclick="agregarACompletadas(${index})">Completar</button>
-        </td>
-      </tr>`;
-    });
+    for (let i = 0; i < texto.clientes.length; i++) {
+      let cliente = texto.clientes[i];
+      html += `
+    <tr>
+    <th scope="row">${[i + 1]}</th>
+      <td><strong>Id :</strong> ${cliente.id}</td>
+      <td><strong>Nombre :</strong> ${cliente.nombre}</td>
+      <td><strong>Cedula:</strong> ${cliente.cedula}</td>
+    `;
 
-    tablaHTML += '</table>';
-
-    // Actualizar el contenido del elemento con ID 'contenedorTabla'
-    document.getElementById('contenedorTabla').innerHTML = tablaHTML;
-
-    // Imprimir el array en la consola
-    console.log(tarea.sinCompletar);
-
-    // Limpiar el input
-    document.getElementById('anadirInput').value = '';
-  } else {
-    console.log('El input está vacío.');
-  }
-}
-
-// Función para borrar el contenido del input
-function borrarFunc() {
-  inputAnadirField.value = "";
-}
-
-// Función para mover una tarea de 'sinCompletar' a 'Completadas'
-function agregarACompletadas(index) {
-  // Añadir la tarea completada al array 'Completadas'
-  tarea.Completadas.push(tarea.sinCompletar[index]);
-  console.log(tarea.Completadas.length + " actualizada lista de completadas");
-
-  // Eliminar la tarea de 'sinCompletar'
-  tarea.sinCompletar.splice(index, 1);
-
-  // Reconstruir la tabla después de la eliminación
-  let tablaHTML = '<table><tr><th>#</th><th>Tarea</th><th>Opciones</th></tr>';
-  
-  tarea.sinCompletar.forEach((element, i) => {
-    tablaHTML += `<tr>
-      <th scope="row">${i + 1}</th>
-      <td>${element}</td>
-      <td><button type="button" class="btn btn-outline-secondary btn-block" onclick="agregarACompletadas(${i})">Completar</button>
-      </td>
-    </tr>`;
-  });
-
-  tablaHTML += '</table>';
-
-  // Actualizar el contenido del elemento con ID 'contenedorTabla'
-  document.getElementById('contenedorTabla').innerHTML = tablaHTML;
-}
-
-// Función para mostrar las tareas completadas
-// Función para mostrar las tareas completadas
-function tareasCompletadas() {
-  // Verificar si la variable 'tarea' y la propiedad 'Completadas' existen
-  if (tarea && tarea.Completadas) {
-    // Verificar si hay tareas completadas
-    if (tarea.Completadas.length > 0) {
-      let listaHTML = '<ul>';
-      tarea.Completadas.forEach((element, index) => {
-        listaHTML += `<li>${index + 1}. ${element}</li>`;
-      });
-      listaHTML += '</ul>';
-
-      // Actualizar el contenido del modal
-      document.getElementById('completedTasksList').innerHTML = listaHTML;
-
-      // Mostrar el modal
-      $('#completedTasksModal').modal('show');
-    } else {
-      let emptyMessage = "Cuando complete alguna tarea esta aparecerá aquí";
-      document.getElementById('completedTasksList').innerHTML = `<p>${emptyMessage}</p>`;
-      $('#completedTasksModal').modal('show');
+      for (let j = 0; j < cliente.vehiculos.length; j++) {
+        let vehiculo = cliente.vehiculos[j];
+        html += `
+        <td><strong>Placa:</strong> ${vehiculo.placa}</td>
+        <td><strong>Ticket:</strong> ${vehiculo.numero_ticket}</td>
+        <td><strong>Prestamo:</strong> ${vehiculo.prestamo}</td>
+        </tr>
+        </br>
+      `;
+      }
     }
+    contenedorTabla.innerHTML = html;
   } else {
-    let errorMessage = "La variable 'tarea' o su propiedad 'Completadas' no existe";
-    document.getElementById('completedTasksList').innerHTML = `<p>${errorMessage}</p>`;
-    $('#completedTasksModal').modal('show');
+    console.log("No hay debedores")
+    alert("No hay debedores");
+  }
+} */
+
+// función con While habilitada
+function myFunction() {
+  if (texto.clientes.length !== 0) {
+    let contenedorTabla = document.getElementById('contenedorTabla');
+    let i = 0;
+    let html = `<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Cedula</th>
+          <th scope="col">Placa</th>
+          <th scope="col">Ticket</th>
+          <th scope="col">Tipo</th>
+          <th scope="col">Opciones</th>
+        </tr>
+      </thead>
+      <tbody>`;
+
+    while (i < texto.clientes.length) {
+      let cliente = texto.clientes[i];
+      html += `
+        <tr class="animate__fadeInDown" style="transition-delay: 1s transition: all 1s; transition-delay: 1s;"">
+          <th scope="row">${cliente.id}</th>
+          <td>${cliente.nombre}</td>
+          <td>${cliente.cedula}</td>
+          <td>${cliente.vehiculos[0].placa}</td>
+          <td>${cliente.vehiculos[0].numero_ticket}</td>
+          <td>${cliente.vehiculos[0].prestamo}</td>
+          <td><a href="#" disabled>Ver</a>/<a href="#" disabled>Editar</a></td>
+        </tr>`;
+      i++;
+    }
+
+    html += `</tbody></table>`;
+    contenedorTabla.innerHTML = html;
+  } else {
+    console.log("No hay deudores");
+    alert("No hay deudores");
   }
 }
 
-// Botones de cerrar
-const modalCloseXButton = document.querySelector(".close")
-modalCloseXButton.addEventListener("click", function (e) {
-  $('#completedTasksModal').modal('hide');
-});
-
-const modalCloseXButton2= document.querySelector(".close2Btn");
-modalCloseXButton2.addEventListener("click", function (e) {
-  $('#completedTasksModal').modal('hide');
-});
